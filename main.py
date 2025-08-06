b322fad2c6a774262193212bf49504f149cc9ff0
jerryx5379 <158000978+jerryx5379@users.noreply.github.com>
2025-08-06 18:33:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,6 @@ This file creates the application and starts the event loop
 It also sets application level details like system name of this app, application style
 """
 
-# TODO: add load model and unload model on program start and end
-# TODO: add save chat on program close
-# TODO: add a icon with pyinstaller when making the exe     pyinstaller --onefile --windowed --icon="assets/icons/app_icon.ico" main.py
-
-
-# This version TODO: add the the rag search button
-# first maybe change it so there is a "tool"... ehh idk
-# add a file button in the inputarea, 
-# on button click, open another window that has folders of files that can be rag searched through. 
-# have text parsers for:  pdf and docx
-
-# Plan:
-# first create a script that can take in files, create vector embeddings, then rag search that
-
 
 import sys
 
